1649eea0b6d882fb1e684febdb7faafae4162ca6
riquelmefreitas193-blip <riquelmefreitas193@gmail.com>
2026-04-10 15:52:21 -0300

    Create questao4.ts
    Resolução incompleta da questão 4 condicional

---
+++ src/condicionais/questao4.ts
@@ -0,0 +1,107 @@
+// 4. Ler dois valores e imprimir uma das três mensagens a seguir:
+// ● ‘Números iguais’, caso os números sejam iguais;
+// ● ‘Primeiro é maior’, caso o primeiro seja maior que o segundo;
+// ● ‘Segundo maior’, caso o segundo seja maior que o primeiro.
+
+let numero1: number = Number(prompt("Digite um número:"))
+let numero2: number = Number(prompt("Digite outro número: "))
+
+if(numero1 == numero2){
+    console.log("Números iguais")
+}
+else if(numero1 > numero2 || numero2 > numero1){
+    console.log("Primeiro é maior")
+}
+
+else()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
