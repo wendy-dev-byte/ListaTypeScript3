f2ef137e370c01af27178dde776436d1241519b3
riquelmefreitas193-blip <riquelmefreitas193@gmail.com>
2026-04-15 10:17:02 -0300

    Create questao3.ts
    resolução incompleta da questão 3 de condicional

---
+++ src/condicionais/questao3.ts
@@ -0,0 +1,14 @@
+// Crie um programa que solicite dois números e simule um menu de uma calculadora:
+// 1 - Soma
+// 2 - Subtração
+// 3 - Multiplicação
+// 4 - Divisão
+// Use switch...Case
+
+let num1: number = Number(prompt("Digite um número: "))
+let num2: number = Number(prompt("Digite outro número: "))
+
+switch(num1,num2)
+{
+    case
+}
