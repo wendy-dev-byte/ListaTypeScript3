c1c4dd47842c6cef3ab3519bf824a597c62cb1fb
riquelmefreitas193-blip <riquelmefreitas193@gmail.com>
2026-04-22 09:59:50 -0300

    Create questao5.ts
    Resolução incompleta da questão 5 de repetições

---
+++ src/repeticoes/questao5.ts
@@ -0,0 +1,26 @@
+// 5. Analisador de Desempenho de Vendas (Relatório de Equipe)
+// Imagine que você está gerindo uma equipe de 5 vendedores.
+//  Função classificarVenda(valor):
+// o Se valor &lt; 1000: Retorna &quot;Bronze&quot;
+// o Se valor entre 1000 e 5000: Retorna &quot;Prata&quot;
+// o Se valor &gt; 5000: Retorna &quot;Ouro&quot;
+//  O Programa:
+// 1. Use um laço for para pedir o valor da venda de cada um dos 5 vendedores.
+// 2. Dentro do laço, chame a função de classificação.
+// 3. Ao final, o programa deve exibir:
+//  O total de vendas da equipe (soma).
+//  Quem foi o vendedor com a maior venda (maior valor digitado).
+//  Quantos vendedores atingiram a categoria &quot;Ouro&quot;.
+
+function classificarVenda(valor: number){
+    if(valor < 1000){
+        return "Bronze"
+    }
+    else if(valor >= 1000 || valor < 5000){
+        return "Prata"
+    }
+    else{
+        return "Ouro"
+    }
+}
+for(let i = 0,)
