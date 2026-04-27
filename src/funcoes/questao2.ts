2ebd796a37b4e47890a266dbded8954dca5e026d
wendy-dev-byte <mileachristibarbosa@gmail.com>
2026-04-26 23:49:39 -0300

    Create questao2.ts
    resolução de função da questão 2

---
+++ src/funcoes/questao2.ts
@@ -0,0 +1,40 @@
+// 2. O IF tem uma estufa e o professor de biologia quer um sistema simples para monitorar a
+// temperatura.
+// ● Crie uma função ler_temperatura():
+// o Simula a leitura de um sensor, pedindo ao usuário para digitar uma temperatura
+// (valor inteiro ou float).
+// o Retorna a temperatura lida.
+// ● Crie uma função verificar_alerta_temperatura(temperatura):
+// o Recebe a temperatura.
+// o Se a temperatura for menor que 10 ou maior que 30, retorna True (alerta).
+// o Caso contrário, retorna False.
+// ● Crie uma função emitir_mensagem_alerta(status_alerta):
+// o Recebe o status_alerta (True ou False).
+// o Se status_alerta for True, imprime &quot;ALERTA: Temperatura fora da faixa ideal!&quot;.
+// o Se status_alerta for False, imprime &quot;Temperatura dentro da faixa normal.&quot;.
+
+function ler_temperatura(){
+    let temperatura: number = Number(prompt("Qual a temperatura do ambiente?"))
+    return temperatura
+}
+function verificar_alerta_temperatura(temperatura:number): boolean{
+    if(temperatura < 10 || temperatura > 30){
+        return true
+    }
+    else{
+        return false
+    }
+}
+function emitir_alerta(status_alerta: boolean){
+    
+    if(status_alerta === true){
+        console.log('ALERTA: Temperatura fora da faixa ideal!')
+    }
+    else{
+        console.log('Temperatura dentro da faixa normal')
+    }
+
+}
+ler_temperatura()
+let ver = verificar_alerta_temperatura(ler_temperatura())
+emitir_alerta(ver)
