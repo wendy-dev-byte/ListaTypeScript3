46851d3b0bab21888c5ab0fd9eaaf9725906719e
wendy-dev-byte <mileachristibarbosa@gmail.com>
2026-04-30 20:39:15 -0300

    Create questao2.ts

---
+++ src/repeticoes/questao2.ts
@@ -0,0 +1,6 @@
+// 2. Desenvolva a tabuada de um número usando for.
+
+for(let i = 0; i < 10; i ++){
+    let digite:number = Number(prompt("Digite um número?"))
+    console.log(digite + "x" + digite*i)
+}
