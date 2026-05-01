1f9b10df346074b21373f97af79cd151af76387c
wendy-dev-byte <mileachristibarbosa@gmail.com>
2026-05-01 12:08:14 -0300

    REPETIÇÃO QUESTAO 1

---
+++ src/repeticoes/questao1.ts
@@ -0,0 +1,15 @@
+// 1. Crie um programa que peça números até o usuário digitar 0 e mostre:
+//  Quantidade de números digitados
+//  Soma total
+
+export function repeticao1 (): void{
+let op =  -1; let cont = 0; let acum = 0
+    while(op != 0){
+        op = Number(prompt('Digite números'))
+        cont++
+        acum += op
+
+    }
+console.log("Quantidade de números: " + cont)
+console.log('A soma dos números: ' + acum)
+}
