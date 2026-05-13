1543c8718973c90f16e73d8cde93e149f586b4c4
riquelmefreitas193-blip <riquelmefreitas193@gmail.com>
2026-05-13 10:26:15 -0300

    treinamento de modularização

---
+++ src/array/MODALIRAZAÇÃO/soma.ts
@@ -0,0 +1,3 @@
+// function soma (num1: number, num2:number){
+
+// }
---
+++ src/array/MODALIRAZAÇÃO/teste.ts
@@ -0,0 +1,7 @@
+console.log("Menu: 1 soma, 2 subtração, 3 divisão, 4 multiplicação")
+let op: number = Number(prompt("Qual operação você escolhe? "))
+let num1:number = Number(prompt("Digite um número? "))
+
+if(op === 1){
+
+}
