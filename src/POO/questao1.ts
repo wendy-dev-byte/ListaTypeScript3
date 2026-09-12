548bba2b2a4e15a445eeeca94d824da68ce90454
wendy-dev-byte <mileachristibarbosa@gmail.com>
2026-09-11 21:17:36 -0300

    Create questao1.ts

---
+++ src/POO/questao1.ts
@@ -0,0 +1,18 @@
+// 1. Classe Bola: Crie uma classe que modele uma bola:
+//  Atributos: Cor, circunferência, material
+//  Métodos: trocaCor e mostraCor
+class Bola {
+    constructor(cor, circunferencia, material) {
+        this.cor = cor;
+        this.circunferencia = circunferencia;
+        this.material = material;
+    }
+    trocaCor(novaCor) {
+        this.cor = novaCor;
+    }
+    mostrarCor() {
+        console.log("a Nova cor é: " + this.cor);
+    }
+}
+export 
+
