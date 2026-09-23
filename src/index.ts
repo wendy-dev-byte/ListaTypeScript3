237c572c4bc3d080870ab90153329f3a6c765818
wendy-dev-byte <mileachristibarbosa@gmail.com>
2026-09-23 17:36:32 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,59 @@
-import { questao1 as rep1 } from "./repeticoes/questao1.js";
-document.getElementById("btn1")?.addEventListener("click",rep1)
+import { questao2 as poo2 } from "./POO/QUESTAO2.js";
+document.getElementById("btn2")?.addEventListener("click",poo2)
 
-import { questao2 as rep2 } from "./repeticoes/questao2.js";
-document.getElementById("btn2")?.addEventListener("click",rep2)
+import { questao3 as poo3} from "./POO/questao3.js";
+document.getElementById("btn3")?.addEventListener("click",poo3)
 
-import { questao3 as rep3}  from "./repeticoes/questao2.js";
+import { questao4 as poo4} from "./POO/questao4.js";
+document.getElementById("btn4")?.addEventListener("click",poo4)
+
+import { questao5 as poo5} from "./POO/questao5.js";
+document.getElementById("btn5")?.addEventListener("click",poo5)
+
+import { questao6 as poo6} from "./POO/questao6.js";
+document.getElementById("btn6")?.addEventListener("click",poo6)
+
+import { questao7 as poo7} from "./POO/questao7.js";
+document.getElementById("btn7")?.addEventListener("click",poo7)
+
+import { questao8 as poo8} from "./POO/questao8.js";
+document.getElementById("btn8")?.addEventListener("click",poo8)
+
+import { questao9 as poo9} from "./POO/questao9.js";
+document.getElementById("btn9")?.addEventListener("click",poo9)
+
+import { questao10 as poo10} from "./POO/questAO10.js";
+document.getElementById("btn10")?.addEventListener("click",poo10)
+
+import { questao11 as poo11} from "./POO/questao11.js";
+document.getElementById("btn11")?.addEventListener("click",poo11)
+
+import { questao12 as poo12} from "./POO/questao12.js";
+document.getElementById("btn12")?.addEventListener("click",poo12)
+
+import { questao13 as poo13} from "./POO/questao13.js";
+document.getElementById("btn13")?.addEventListener("click",poo13)
+
+import { questao14 as poo14} from "./POO/questao14.js";
+document.getElementById("btn")?.addEventListener("click",poo14)
+
+import { questao15 as poo15} from "./POO/questao15.js";
+document.getElementById("btn15")?.addEventListener("click",poo15)
+
+import { questao16 as poo16} from "./POO/questaoo16.js";
+document.getElementById("btn16")?.addEventListener("click",poo16)
+
+import { questao17 as poo17} from "./POO/questao17.js";
+document.getElementById("btn17")?.addEventListener("click",poo17)
+
+import { questao19 as poo19} from "./POO/questao19.js";
+document.getElementById("btn19")?.addEventListener("click",poo19)
+
+import { questao20 as poo20} from "./POO/questao20.js";
+document.getElementById("btn20")?.addEventListener("click",poo20)
+
+import { questao22 as poo22} from "./POO/questao22.js";
+document.getElementById("btn22")?.addEventListener("click",poo22)
+
+import { questao31 as poo31} from "./POO/questao31.js";
+document.getElementById("btn31")?.addEventListener("click",poo31)
