928dbd21cf85ada43b9681b5822f754e64d58acf
wendy-dev-byte <mileachristibarbosa@gmail.com>
2026-09-23 17:36:27 -0300

    Delete index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { questao1 as rep1 } from "./repeticoes/questao1.js";
-rep1();
-document.getElementById("btn1")?.addEventListener("click",rep1)
-
-import {questao2 as rep2} from "./repeticoes/questao2.js";
-rep2();
-document.getElementById("btn2")?.addEventListener("click",rep2)
